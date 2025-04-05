3b839fc03b22209621459d6cf43e8969e3b3d400
Anvith Shenoy B <anvithshenoyb@gmail.com>
2025-04-05 20:56:28 +0530

    Cors modified to accept all

--- server.js
+++ server.js
@@ -21,11 +21,7 @@ if (cluster.isPrimary) {
 	})
 } else {
 	const app = express()
-	app.use(
-		cors({
-			origin: process.env.FRONTEND_URL || "*", // Replace with your frontend URL for security
-		})
-	)
+	app.use(cors())
 	app.use(express.json())
 
 	mongoose
@@ -38,6 +34,6 @@ if (cluster.isPrimary) {
 
 	const PORT = process.env.PORT ?? 3000
 	app.listen(PORT, () => {
-		console.log(`🚀 Worker ${process.pid} running at http://localhost:${PORT}`)
+		console.log(`🚀 Worker ${process.pid} running on ${PORT}`)
 	})
 }
