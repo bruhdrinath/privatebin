aeaca4106bc7bc4ddf771afc207786756d7c79c4
Anvith Shenoy B <anvithshenoyb@gmail.com>
2025-04-09 23:10:40 +0530

    heartbeat func had env issues: resolved

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import cors from "cors"
+import dotenv from "dotenv"
 import express from "express"
 import pasteRoutes from "./routes/pasteRoutes.js"
 
 const app = express()
+dotenv.config()
 
 // Middlewares
 app.use(cors())
@@ -20,7 +22,8 @@ app.get("/", (req, res) => {
 // Routes
 app.use("/api", pasteRoutes)
 
-// Self-ping every 12 minutes to keep the app alive
+// Self-ping every 12 minutes to keep the app alive or use https://uptimerobot.com/ for uptime monitoring
+// This is a workaround for platforms like Render, which require a ping to keep the app alive
 setInterval(() => {
 	fetch(process.env.ORIGIN)
 		.then(() => console.log("🟢 Self-ping successful"))
